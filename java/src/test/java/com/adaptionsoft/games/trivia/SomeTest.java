5f295f4c7b36908f4c140cde79e030c15a6ae945
Eric Smith <esmith@8thlight.com>
2014-11-20 16:11:50 -0600

    Fix the test - make it pass

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -2,12 +2,14 @@ package com.adaptionsoft.games.trivia;
 
 import static org.junit.Assert.*;
 
+import com.adaptionsoft.games.trivia.runner.GameRunner;
 import org.junit.Test;
 
 public class SomeTest {
 
 	@Test
 	public void true_is_true() throws Exception {
-		assertTrue(false);
+        GameRunner runner = new GameRunner();
+        assertTrue(true);
 	}
 }
